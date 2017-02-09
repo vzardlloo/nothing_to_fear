17257ff9c479b5d1efa172d7169ae321010e9e2c
Hu Jun <hujun123qwe@163.com>
2017-02-09 22:50:06 +0800

    2017/02/09:完善登录界面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,18 +33,18 @@
 		            <span class="icon-bar"></span>
 		            <span class="icon-bar"></span>
         		</button>
-        		<a class="navbar-brand" href="http://www.creative-tim.com">啄木鸟农业</a>
+        		<a class="navbar-brand" href="http://x.ploveruav.com">啄木鸟农业</a>
         	</div>
 
         	<div class="collapse navbar-collapse" id="navigation-example">
         		<ul class="nav navbar-nav navbar-right">
 					<li>
-    					<a href="../components-documentation.html" target="_blank">
+    					<a href="#" target="_blank">
     						查看正在作业
     					</a>
     				</li>
     				<li>
-						<a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
+						<a href="#" target="_blank">
 							<i class="material-icons">unarchive</i> 关于啄木鸟农业
 						</a>
     				</li>
@@ -54,7 +54,7 @@
     </nav>
 
     <div class="wrapper">
-		<div class="header header-filter" style="background-image: url('assets/img/city.jpg'); background-size: cover; background-position: top center;">
+		<div class="header header-filter" style="background-image: url('assets/img/bg5.jpg'); background-size: cover; background-position: top center;">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
@@ -62,21 +62,13 @@
 							<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         		{{ csrf_field() }}
 								<div class="header header-primary text-center">
-									<h4>登 录</h4>
+									<h3>登 录</h3>
 									<div class="social-line">
-										<a>啄木鸟农业</a>
+										<h4>啄木鸟农业</h4>
 									</div>
 								</div>
-								<p class="text-divider">精准农业</p>
+								<p class="text-divider">植保信息平台</p>
 								<div class="content">
-
-									<div class="input-group">
-										<span class="input-group-addon">
-											<i class="material-icons">face</i>
-										</span>
-										<input type="text" class="form-control" placeholder="用户名...">
-									</div>
-
 									<div class="input-group">
 										<span class="input-group-addon">
 											<i class="material-icons">email</i>
@@ -116,24 +108,24 @@
 								</a>
 							</li>
 							<li>
-								<a href="http://www.ploveruav.com/about">
+								<a href="http://weixin.ploveruav.com/about.html">
 								   关于我们
 								</a>
 							</li>
 							<li>
-								<a href="http://www.ploveruav.com/team">
-								   啄木鸟团队
+								<a href="http://weixin.ploveruav.com/contact.html">
+									联系我们
 								</a>
 							</li>
 							<li>
-								<a href="http://www.ploveruav.com/contect">
-									联系我们
+								<a href="http://weixin.ploveruav.com/team.html">
+								   啄木鸟团队
 								</a>
 							</li>
 						</ul>
 		            </nav>
 		            <div class="copyright pull-right">
-		                &copy; 2017, made with <i class="fa fa-heart heart"></i> by <a href="http://www.ploveruav.com" target="_blank">Plover UAV</a>
+		                &copy; 2017, made with <i class="fa fa-heart heart"></i> by <a href="http://www.ploveruav.com" target="_blank">PloverUAV</a>
 		            </div>
 		        </div>
 		    </footer>
@@ -148,14 +140,5 @@
 	<script src="/assets/js/jquery.min.js" type="text/javascript"></script>
 	<script src="/assets/js/bootstrap.min.js" type="text/javascript"></script>
 	<script src="/assets/js/material.min.js"></script>
-
-	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-	<script src="/assets/js/nouislider.min.js" type="text/javascript"></script>
-
-	<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
-	<script src="/assets/js/bootstrap-datepicker.js" type="text/javascript"></script>
-
-	<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
 	<script src="/assets/js/material-kit.js" type="text/javascript"></script>
-
 </html>
